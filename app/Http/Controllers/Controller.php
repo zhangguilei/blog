2603075043577628d188c1926d38bad92af038a9
zhangguilei <823461798@qq.com>
2020-11-13 10:12:00 +0800

    blog

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    print_r(45654);exit;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}

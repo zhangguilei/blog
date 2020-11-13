37fc017f2ea16eb679b3370ab42aea9770052e1a
zhangxiaojiu123 <3062788545@qq.com>
2020-11-13 12:03:53 +0800

    aaa

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,6 +9,5 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    print_r(45654);exit;
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
